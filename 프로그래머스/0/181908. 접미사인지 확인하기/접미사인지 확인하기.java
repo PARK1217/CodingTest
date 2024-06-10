d7a05433429b5ff2676389edb8760e69d18d685c
Park <fasosan@gmail.com>
2024-06-10 11:24:35 +0900

    다른방식 추가

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,17 +1,23 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-        int my_string_length = my_string.length();
-        int is_suffix_length = is_suffix.length();
+        // int answer = 0;
+        // int my_string_length = my_string.length();
+        // int is_suffix_length = is_suffix.length();
         
-            if (is_suffix_length > my_string_length) {
-                return 0;
-            }
-            if (my_string.substring(my_string_length - is_suffix_length).equals(is_suffix)) {
-                return 1;
-            }
+        //     if (is_suffix_length > my_string_length) {
+        //         return 0;
+        //     }
+        //     if (my_string.substring(my_string_length - is_suffix_length).equals(is_suffix)) {
+        //         return 1;
+        //     }
         
-        return answer;
+        // return answer;
+
+        if(my_string.endsWith(is_suffix)) {
+            return 1;
+        } else {
+            return 0;
+        }
 
     }
 }
