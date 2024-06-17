322854121104bbc3aac5ac6de67d45514aa18491
PARK1217 <90600169+PARK1217@users.noreply.github.com>
2024-06-17 15:09:06 +0900

    [Bronze V] Title: 꼬마 정민, Time: 204 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+        long l = sc.nextLong();
+        //입력되는 자료형 주의하기
+
+        System.out.println(n + m + l);
+       
+    }
+}
